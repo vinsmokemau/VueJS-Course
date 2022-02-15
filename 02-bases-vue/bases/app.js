3640893e037025019d148b417c611383171e3a31
Mau Munguia <maumg1196@gmail.com>
2022-02-15 13:03:19 -0600

    Add Vue Bases module

---
+++ 02-bases-vue/bases/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    // template: `
+    // <h1>Hola Mundo</h1>
+    // <p>Desde app.js</p>
+    // `
+
+    data() {
+        return {
+            quote: "I'm Batman",
+            author: "Bruce Wayne"
+        }
+    },
+    methods : {
+        changeQuote() {
+            console.log("Hello World!")
+            this.author = "Batman"
+            this.capitalize()
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase()
+        }
+    }
+})
+
+
+app.mount('#myApp')
