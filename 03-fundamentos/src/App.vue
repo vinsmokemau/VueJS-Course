b256509911555b448c5c192d0056ad947eb1f853
Mau Munguia <maumg1196@gmail.com>
2022-02-15 16:39:20 -0600

    Add section 3 fundamentos

---
+++ 03-fundamentos/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Counter title="Entraron" :start="10"/>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  components: { Counter },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,64 @@
+<template>
+  <h2>{{ getTitle }}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <div>
+      <button @click="increaseCounter">+1</button>
+      <button @click="decreaseCounter">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            default: 1,
+            // required: true,
+            validator( value ){
+                return value > 0
+            }
+        },
+    },
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        getSquare() {
+            return this.counter * this.counter
+        },
+        increaseCounter() {
+            this.counter++
+        },
+        decreaseCounter() {
+            this.counter--
+        },
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter
+        },
+        getTitle(){
+            return this.title || "Counter"
+        }
+    }
+}
+</script>
+
+<style>
+    button{
+        background-color: #64BB87;
+        border-radius: 5px;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
+    button:hover {
+        background-color: #5aa67b;
+        transition: 0.3s ease-in-out;
+    }
+</style>
